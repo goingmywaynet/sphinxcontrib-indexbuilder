c25b8f4926aba8a3855ac38ed6749c775e18bbb3
Joey Chen <joey-tech@goingmyway.net>
2018-10-02 10:18:48 +0900

    プレリリース版 sphinxのextディレクトリに入れておけば、tagfinderが利用できる状態

--- tests/tmp/conf.py
+++ tests/tmp/conf.py
@@ -105,6 +105,9 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'testprojectdoc'
 
+html_search_options = {
+      'type': 'janome'
+}
 
 # -- Options for LaTeX output ------------------------------------------------
 
